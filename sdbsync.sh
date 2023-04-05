dcb3646e8927cda1a8c34681d9fec7a9f16e0e35
someone <someone@macbook.local>
2023-04-06 02:06:30 +0300

    sdbsync.sh

--- sdbsync.sh
+++ sdbsync.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-sd1_path="/Volumes/SD1"
-sd2_path="/Volumes/SD2"
-db="secrets.kdbx"
+sd1="/Volumes/SD1"
+sd2="/Volumes/SD2"
+db="oops.kdbx"
+
+function hash {
+	ds1_hash=$(md5 $sd1/$db | awk '{print$4}')
+	echo "SD1 = ${ds1_hash}"
+	ds2_hash=$(md5 $sd2/$db | awk '{print$4}')
+	echo "SD2 = ${ds2_hash}"
+}
 
 if [ -d $sd1_path ] && [ -d $sd2_path ]; then
-	echo "Both cards is available"
-	echo $(md5  $sd1_path/$db)
-	echo $(md5  $sd2_path/$db)
-	read -p "Сan we start syncing? [y/n]: " sync
-	if [ $sync = "y" ]; then
-		cp -f $sd1_path/$db $sd2_path
-		echo $(md5  $sd1_path/$db)
-		echo $(md5  $sd2_path/$db)
-	elif [ $sync = "n" ]; then
-		echo "Stop"
+	echo '' ; echo "Nice! Both cards is available."
+	if [ $ds1_hash = $ds2_hash]; then
+		echo '' ; hash
+		echo '' ; echo "Oh, wow. No need to update. Lovely." ; echo ''
 	else
-		echo "Read carefully next time, bitch"
+		echo '' ; hash
+		echo '' ; read -p "Сan we start syncing? [y/n]: " sync
+		if [ $sync = "y" ]; then
+			cp -f $sd1/$db $sd2
+			echo '' ; hash
+		elif [ $sync = "n" ]; then
+			echo '' ; echo "Well... bye." ; echo ''
+		else
+			echo '' ; echo "Read carefully next time, man." ; echo ''
+		fi
 	fi
 else
-	echo "One or more cards are not available"
+	echo '' ; echo "Oh, shit... one or more cards are not available." ; echo ''
 fi
